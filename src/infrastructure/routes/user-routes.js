36bc2bc2b2071e0647c56ab26d9ee44654fb2dea
xllRoku <jhinlmlz@gmail.com>
2022-11-28 17:38:44 -0500

    add user login route and controller

--- src/infrastructure/routes/user-routes.js
+++ src/infrastructure/routes/user-routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { userLoginController } from "../controllers/user-login.controller.js";
 import { userRegisterController } from "../controllers/user-register.controller.js";
 
 const router = Router();
 
 router.post("/register", userRegisterController);
+router.post("/login", userLoginController);
 
 export default router;
