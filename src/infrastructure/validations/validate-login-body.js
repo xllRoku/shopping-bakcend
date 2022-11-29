c5167a96cf96657789c8d17223a48ef65a297e38
xllRoku <jhinlmlz@gmail.com>
2022-11-29 14:39:13 -0500

    add error

--- src/infrastructure/validations/validate-login-body.js
+++ src/infrastructure/validations/validate-login-body.js
@@ -1,14 +1,17 @@
+import { UnauthorizedException } from "../../application/errors/unauthorized.exception.js";
 import {
   validateEmail,
   validatePassword,
 } from "../../domain/validations/user-validation.js";
+import { BadRequestException } from "./errors/bad-request.exception.js";
 
 export const validateLoginBody = (body) => {
   const { email, password } = body;
-  if (!email || !password) throw new Error("Se espera un email y contraseña");
+  if (!email || !password)
+    throw new BadRequestException("Se espera un email y contraseña");
 
   if (!validateEmail(email) || validatePassword(password))
-    throw new Error("Las credenciales son incorrectas");
+    throw new UnauthorizedException("Las credenciales son incorrectas");
 
   return { email, password };
 };
